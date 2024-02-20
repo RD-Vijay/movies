1e3671e6f1b34f338288706d53b8c3cb9f637f01
Vijay Bagdi <160385614+RD-Vijay@users.noreply.github.com>
2024-02-20 13:04:57 +0530

    added files

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoFakeAPI
+{
+    internal class Program
+    {
+        public static int Areacircle(int x)
+        {
+            if (x == 2)
+            {
+                return 2;
+            }
+            else
+                return 4;
+        }
+
+        public static void main(String args[])
+        {
+            Areacircle(2);
+        }
+    }
+}
